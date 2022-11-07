1f548677cb8ae802a348f9dbbb948654f12103b9
exoxaierist <exoxaierist@naver.com>
2022-11-07 15:16:37 +0900

    fix fix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.117.1/build/three.module.js';
 
 let cards = document.querySelectorAll('#card');
 let elements = document.querySelectorAll('#element');
